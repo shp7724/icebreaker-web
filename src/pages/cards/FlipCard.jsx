8d21c18572bcf7269247315ad03ae5c7825d73fa
shinhong-park <shinhong.park@linecorp.com>
2024-02-17 19:58:31 +0900

    Implement drag gesture

--- src/pages/cards/FlipCard.jsx
+++ src/pages/cards/FlipCard.jsx
@@ -2,35 +2,37 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 import classNames from 'classnames';
 const transition = { type: 'spring', stiffness: 140, damping: 15 };
+const rotateDegrees = [7, 0, -7, 5, -5];
 
-const FlippableCard = ({ index }) => {
+const FlippableCard = ({ index, zIndex, handleDragEnd }) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
     const flipCard = () => {
         setIsFlipped(!isFlipped);
     };
-    console.log(index);
 
-    const rotateDegress = [7, 0, -7, 5, -5]
 
     return (
-        <div className={classNames(`flex justify-center`)}
+        <motion.div
+            id={`flippable-card-${index}-${zIndex}`}
+            className={classNames('flex justify-center')}
+            drag
+            onDragEnd={() => handleDragEnd(index)}
+            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+            dragElastic={1}
+            style={{ rotate: rotateDegrees[index % rotateDegrees.length], zIndex: zIndex }}
         >
-            <div style={{
-                transform: `rotate(${rotateDegress[index]}deg)`,
-            }}>
-                <motion.div
-                    className="w-64 h-64 relative text-white flex justify-center items-center cursor-pointer"
-                    onClick={flipCard}
-                    animate={{ rotateY: isFlipped ? 180 : 0 }}
-                    transition={transition}
-                    style={{ transformStyle: 'preserve-3d' }}
-                >
-                    <CardSide className='rotate-y-0 bg-red-500' />
-                    <CardSide className='rotate-y-180 bg-blue-500' />
-                </motion.div>
-            </div>
-        </div >
+            <motion.div
+                className="w-64 h-64 relative text-white flex justify-center items-center cursor-pointer"
+                onTap={flipCard}
+                animate={{ rotateY: isFlipped ? 180 : 0 }}
+                transition={transition}
+                style={{ transformStyle: 'preserve-3d' }}
+            >
+                <CardSide className='rotate-y-0 bg-red-500'>{index} {zIndex}</CardSide>
+                <CardSide className='rotate-y-180 bg-blue-500' >{index} {zIndex}</CardSide>
+            </motion.div>
+        </motion.div>
     );
 };
 
--- src/pages/cards/ResultPage.jsx
+++ src/pages/cards/ResultPage.jsx
@@ -1,45 +1,41 @@
 
 import logo from '../../../static/friend_circle.svg';
 import FlippableCard from './FlipCard';
-const ResultPage = () => {
+import { useEffect, useState } from 'react';
 
-    const cards = [
-        {
-            id: 1,
-            title: 'Card 1',
-            description: 'Description 1',
-            cardDescription: 'Card Description 1',
-            coverImg: logo,
-        },
-        {
-            id: 2,
-            title: 'Card 2',
-            description: 'Description 2',
-            cardDescription: 'Card Description 2',
-            coverImg: logo,
-        },
-        {
-            id: 3,
-            title: 'Card 3',
-            description: 'Description 3',
-            cardDescription: 'Card Description 3',
-            coverImg: logo,
-        },
-    ]
+const ResultPage = () => {
+    const [cards, setCards] = useState([
+        { id: 1, title: 'Card 1', description: 'Description 1', cardDescription: 'Card Description 1', coverImg: logo },
+        { id: 2, title: 'Card 2', description: 'Description 2', cardDescription: 'Card Description 2', coverImg: logo },
+        { id: 3, title: 'Card 3', description: 'Description 3', cardDescription: 'Card Description 3', coverImg: logo },
+    ]);
+
+    const [zIndices, setZIndices] = useState(Array(cards.length).fill(0).map((_, i) => i));
+
+    const handleDragEnd = (cardIndex) => {
+        setZIndices(prevZIndices => {
+            return prevZIndices.map((zIndex, index) => {
+                if (index === cardIndex) return 0;
+                if (zIndex < prevZIndices[cardIndex]) return zIndex + 1;
+                return zIndex;
+            });
+        });
+    }
 
     return (
         <div className='flex justify-center items-center h-full'>
-
             <div className='grid'>
-                {cards.map((card, i) => {
-                    return (
-                        <div key={card.id} className='' style={{ gridColumn: 1, gridRow: 1 }}>
-                            <FlippableCard index={i} />
-                        </div>
-                    )
-                })}
+                {cards.map((card, i) => (
+                    <div key={card.id} style={{ gridColumn: 1, gridRow: 1, zIndex: zIndices[i] }}>
+                        <FlippableCard
+                            index={i}
+                            zIndex={zIndices[i]}
+                            handleDragEnd={handleDragEnd}
+                        />
+                    </div>
+                ))}
             </div>
-        </div >
+        </div>
     );
 }
 
