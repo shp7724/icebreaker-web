bbb3b6460183a962236038ab03fc70f0df6a32f1
shinhong-park <shinhong.park@linecorp.com>
2024-02-17 23:22:34 +0900

    Add cards svgs

--- src/pages/cards/FlipCard.jsx
+++ src/pages/cards/FlipCard.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 const transition = { type: 'spring', stiffness: 140, damping: 15 };
 const rotateDegrees = [7, 0, -7, 5, -5];
-
-const FlippableCard = ({ index, zIndex, handleDragEnd }) => {
+const FlippableCard = ({ index, zIndex, handleDragEnd, card }) => {
     const [isFlipped, setIsFlipped] = useState(false);
 
     const flipCard = () => {
@@ -28,14 +28,19 @@ const FlippableCard = ({ index, zIndex, handleDragEnd }) => {
             style={{ zIndex: zIndex }}
         >
             <motion.div
-                className="w-64 h-64 relative text-white flex justify-center items-center cursor-pointer"
+                className="w-[281px] h-[392px] relative text-white flex justify-center items-center cursor-pointer"
                 onTap={flipCard}
                 animate={{ rotateY: isFlipped ? 180 : 0, scale: isFlipped ? 1.4 : 1 }}
                 transition={transition}
                 style={{ transformStyle: 'preserve-3d' }}
             >
-                <CardSide className='rotate-y-0 bg-red-500'>{index} {zIndex}</CardSide>
-                <CardSide className='rotate-y-180 bg-blue-500' >{index} {zIndex}</CardSide>
+                <CardSide className='rotate-y-0 overflow-hidden'>
+                    <img src={card.coverImg} />
+                </CardSide>
+                <CardSide className='rotate-y-180 overflow-hidden' >
+                    <div className='bg-[#5C739B] w-full h-full'>
+                    </div>
+                </CardSide>
             </motion.div>
         </motion.div>
     );
@@ -44,12 +49,24 @@ const FlippableCard = ({ index, zIndex, handleDragEnd }) => {
 const CardSide = ({ children, className }) => {
     return (
         <motion.div
-            className={classNames('absolute w-full h-full bg-blue-500 top-0 left-0 rounded-2xl shadow-lg', className)}
+            className={classNames('absolute w-full h-full top-0 left-0 rounded-2xl shadow-lg', className)}
             style={{ backfaceVisibility: 'hidden' }}
         >
             {children}
         </motion.div>
     )
 }
+
+FlippableCard.propTypes = {
+    index: PropTypes.number.isRequired,
+    zIndex: PropTypes.number.isRequired,
+    handleDragEnd: PropTypes.func.isRequired,
+    card: PropTypes.object.isRequired
+};
+
+CardSide.propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string
+};
 
 export default FlippableCard;
--- src/pages/cards/ResultPage.jsx
+++ src/pages/cards/ResultPage.jsx
@@ -1,15 +1,8 @@
-
-import logo from '../../../static/friend_circle.svg';
 import FlippableCard from './FlipCard';
 import { useState } from 'react';
+import cards from './cards';
 
 const ResultPage = () => {
-    const [cards, setCards] = useState([
-        { id: 1, title: 'Card 1', description: 'Description 1', cardDescription: 'Card Description 1', coverImg: logo },
-        { id: 2, title: 'Card 2', description: 'Description 2', cardDescription: 'Card Description 2', coverImg: logo },
-        { id: 3, title: 'Card 3', description: 'Description 3', cardDescription: 'Card Description 3', coverImg: logo },
-    ]);
-
     const [zIndices, setZIndices] = useState(Array(cards.length).fill(0).map((_, i) => i));
 
     const handleDragEnd = (cardIndex) => {
@@ -23,13 +16,17 @@ const ResultPage = () => {
     }
 
     return (
-        <div className='flex justify-center items-center h-full'>
+        <div className='flex justify-center items-center flex-col h-full'>
+            <div className='text-slate-500 font-semibold mb-16 text-xl'>
+                두 분의 친구 카드가 도착했어요
+            </div>
             <div className='grid'>
                 {cards.map((card, i) => (
                     <div key={card.id} style={{ gridColumn: 1, gridRow: 1, zIndex: zIndices[i] }}>
                         <FlippableCard
                             index={i}
                             zIndex={zIndices[i]}
+                            card={card}
                             handleDragEnd={handleDragEnd}
                         />
                     </div>
